569f65390858081489e65facb59677392817415e
Shani <4171396sg@gmail.com>
2025-10-16 14:25:25 +0300

    Shani/fix/add files to empty directories

--- server/EnglishGamesPlatform.Backend/obj/Debug/net8.0/EnglishGamesPlatform.Backend.AssemblyInfo.cs
+++ server/EnglishGamesPlatform.Backend/obj/Debug/net8.0/EnglishGamesPlatform.Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnglishGamesPlatform.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b22ed9167f62ef67f6d9624a0c08d9dee3737fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnglishGamesPlatform.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnglishGamesPlatform.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
