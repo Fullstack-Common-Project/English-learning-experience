ff638f5c346c87a001f16c944e2a98a481e1b607
tamar240 <tamar36603@gmail.com>
2025-10-17 14:35:20 +0300

    avital and tamar / Added models, DbContext, migrations

---
+++ server/EnglishGamesPlatform.Backend/Data/AppDbContext.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server.models;
+
+namespace server.data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Word> Words { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<GameResult> GameResults { get; set; }
+        public DbSet<Sentence> Sentences { get; set; }
+        public DbSet<Progress> Progress { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Word>()
+                .HasOne(w => w.Category)
+                .WithMany(c => c.Words)
+                .HasForeignKey(w => w.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Image>()
+                .HasOne(i => i.Word)
+                .WithMany(w => w.Images)
+                .HasForeignKey(i => i.WordId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<GameResult>()
+                .HasOne(gr => gr.Game)
+                .WithMany(g => g.GameResults)
+                .HasForeignKey(gr => gr.GameId);
+
+            modelBuilder.Entity<GameResult>()
+                .HasOne(gr => gr.User)
+                .WithMany(u => u.GameResults)
+                .HasForeignKey(gr => gr.UserId);
+
+            modelBuilder.Entity<Progress>()
+                .HasOne(p => p.Game)
+                .WithMany(g => g.Progress)
+                .HasForeignKey(p => p.GameId);
+
+            modelBuilder.Entity<Progress>()
+                .HasOne(p => p.User)
+                .WithMany(u => u.Progress)
+                .HasForeignKey(p => p.UserId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ server/EnglishGamesPlatform.Backend/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace server.models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required, MaxLength(100)]
+        public string FullName { get; set; } = null!;
+        [Required, EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required, MaxLength(100)]
+        public string Password { get; set; } =null!;
+
+        public ICollection<GameResult> GameResults { get; set; } = new List<GameResult>();
+        public ICollection<Progress> Progress{ get; set; } = new List<Progress>();
+    }
+}
