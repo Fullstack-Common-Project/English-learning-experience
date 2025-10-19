0b07f485bdc9fd71f9b88099f3242fb2bbda460e
sari kahana <sarik0556770565@gmail.com>
2025-10-20 00:52:00 +0300

    login_&_register_ui

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,7 +3,7 @@
 import "./globals.css";
 import { Provider, useDispatch, useSelector } from "react-redux";
 import Header from "@/components/common/Header";
-import { store } from "./store/gameStore";
+import { store } from "../store/gameStore";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 interface User {
   name: string;
