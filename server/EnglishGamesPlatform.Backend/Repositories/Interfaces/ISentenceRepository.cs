ac4f07f0c817300f3ed6d933697f35e2ecb8a17f
tamar240 <tamar36603@gmail.com>
2025-10-20 14:51:33 +0300

    Avital_and_Tamar/feature/add_entities_repositories_models_and_match_interfaces

---
+++ server/EnglishGamesPlatform.Backend/Repositories/Interfaces/ISentenceRepository.cs
@@ -0,0 +1,10 @@
+﻿using EnglishGamesPlatform.Backend.Models.Entities;
+
+namespace EnglishGamesPlatform.Backend.Repositories.Interfaces
+{
+    public interface ISentenceRepository
+    {
+        Task<Sentence?> GetByIdAsync(int id);
+        Task<List<Sentence>> GetRandomSentencesAsync(int count);
+    }
+}
