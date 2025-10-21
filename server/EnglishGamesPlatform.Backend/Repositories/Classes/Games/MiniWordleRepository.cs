0fe2c0da4268e24a32c96d10187b7ec5e227a670
hadar <hadargerashi2005@gmail.com>
2025-10-21 11:17:21 +0300

    create server of mini wordle game

---
+++ server/EnglishGamesPlatform.Backend/Repositories/Classes/Games/MiniWordleRepository.cs
@@ -0,0 +1,32 @@
+﻿using EnglishGamesPlatform.Backend.Models;
+using EnglishGamesPlatform.Backend.Models.DTOs;
+using EnglishGamesPlatform.Backend.Models.GameInitialDatas;
+using EnglishGamesPlatform.Backend.Repositories.Interfaces;
+
+namespace EnglishGamesPlatform.Backend.Repositories.Classes.Games
+{
+    public class MiniWordleRepository : IGeneralGameRepository
+    {
+    
+       
+        private readonly WordRepository _wordRepository;
+        public MiniWordleRepository(WordRepository wordRepository)
+        {
+            _wordRepository = wordRepository;
+        }
+
+        public string GameName => "Mini Wordle";
+
+        public GameInitialData GetData()
+        {
+            string targetWord = _wordRepository.GetRandomWordsAsync(1,3,5).ToString()!;
+            return new MiniWordleData()
+            {
+                WordLength = targetWord.Length,
+                TargetWord = targetWord
+            };
+        }
+
+     
+    }
+}
