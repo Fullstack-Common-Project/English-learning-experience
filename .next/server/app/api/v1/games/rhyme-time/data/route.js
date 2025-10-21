990bc9f651f207e775b33807a7306da1c744f0e4
rikiMaman <148782616+rikiMaman@users.noreply.github.com>
2025-10-21 19:51:07 +0300

    Clean up .next and update ignored files

--- .next/server/app/api/v1/games/rhyme-time/data/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../../../chunks/[turbopack]_runtime.js")("server/app/api/v1/games/rhyme-time/data/route.js")
-R.c("server/chunks/node_modules_next_941350cf._.js")
-R.c("server/chunks/[root-of-the-server]__102d6dc1._.js")
-R.m("[project]/.next-internal/server/app/api/v1/games/rhyme-time/data/route/actions.js [app-rsc] (server actions loader, ecmascript)")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/v1/games/rhyme-time/data/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/v1/games/rhyme-time/data/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
