80bfd432a90d6b585bb9b659a6e45a54da17b325
rikiMaman <148782616+rikiMaman@users.noreply.github.com>
2025-10-21 14:43:33 +0300

    Remove tracked node_modules and build folders

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"grLyM5ahLWJpoS6ZYXKabGUu3BNycYhpZTa8cToS9uA=\"\n}"
--- .next/static/chunks/src_app_page_tsx_3f1a4836._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_83f94edf._.js",
-  "static/chunks/src_f1151bbb._.js"
-],
-    source: "dynamic"
-});
