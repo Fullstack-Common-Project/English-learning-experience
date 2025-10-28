b35e3da5b460a5dfae125d99cac0f441cc76b42e
miryamShapira <mirishapira6260@gmail.com>
2025-10-28 10:01:29 +0200

    PhraseCraze game updates

--- server/EnglishGamesPlatform.Backend/Program.cs
+++ server/EnglishGamesPlatform.Backend/Program.cs
@@ -67,6 +67,8 @@ builder.Services.AddScoped<IGeneralGameRepository, TwinWordsGameRepository>();
 builder.Services.AddScoped<IGeneralGameRepository, DoubleVisionRepository>();
 builder.Services.AddScoped<IGeneralGameRepository, MemoryMatchSynonymsRepository>();
 builder.Services.AddScoped<IGeneralGameRepository, PicPickRepository>();
+builder.Services.AddScoped<IGeneralGameRepository,PhraseCrazeRepository>();
+
 builder.Services.AddScoped<IGeneralGameRepository, MemoryMatchAntonymsRepository>();
 builder.Services.AddScoped<IGeneralGameRepository, GrammarGuruRepository>();
 
@@ -75,10 +77,12 @@ builder.Services.AddScoped<IGeneralGameRepository, GrammarGuruRepository>();
 builder.Services.AddScoped<IGeneralGameRepository,ContextCluesRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IImageRepository, ImageRepository>();
+
 builder.Services.AddScoped<ISentenceRepository, SentenceRepository>();
 builder.Services.AddScoped<IWordRepository, WordRepository>();
 builder.Services.AddScoped<IOppositeWordRepository, OppositeWordRepository>();
 builder.Services.AddScoped<ITwinWordRepository, TwinWordRepository>();
+
 builder.Services.AddScoped<IFakeSentenceRepository, FakeSentenceRepository>();
 builder.Services.AddScoped<IGrammarQuestionRepository, GrammarQuestionRepository>();
 builder.Services.AddScoped<IGeneralGameRepository, GuessMaster20Repository>();
