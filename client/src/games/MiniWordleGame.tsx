514f44cff585146d286b783dea3c977da96cde1f
hadar <hadargerashi2005@gmail.com>
2025-11-02 19:51:32 +0200

    update references for renamed sound file

--- client/src/games/MiniWordleGame.tsx
+++ client/src/games/MiniWordleGame.tsx
@@ -94,7 +94,6 @@ export default function MiniWordleGame({
         <>
             <Button
                 onClick={() => handleGameOver()}
-
             >
                 End Game
             </Button>
